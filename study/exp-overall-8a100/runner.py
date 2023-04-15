d7e13429b5634da6035926fdf6968f06d7bbf2c7
sxn <xiaoniu.sxn@outlook.com>
2023-04-15 06:55:25 +0000

    add 8a100 logfile

--- study/exp-overall-8a100/runner.py
+++ study/exp-overall-8a100/runner.py
@@ -28,8 +28,8 @@ GraphSage
 # 1.1 unsup, large batch
 cur_common_base = (cur_common_base.copy().override('model', [Model.sage]).override('unsupervised', [True]))
 cur_common_base = (cur_common_base.copy().override('batchsize', [2000]).override('local_step', [125]))
-cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.papers100M_undir, ]).override('cache_percent', pg(1,2,1) + pg(4,20,4) + [0.25, 0.40]).override('use_amp', [False]))
-cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.friendster,       ]).override('cache_percent', pg(1,2,1) + pg(4,20,4) + [0.25, 0.40]).override('use_amp', [False]))
+cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.papers100M_undir, ]).override('cache_percent', pg(1,2,1) + pg(4,20,4) + [0.25, 0.40]).override('use_amp', [False]).override('batchsize', [2000, 4000]))
+cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.friendster,       ]).override('cache_percent', pg(1,2,1) + pg(4,20,4) + [0.25, 0.40]).override('use_amp', [False]).override('batchsize', [2000, 4000]))
 # # # 1.2 unsup, mag 240 requires different batch
 # cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.mag240m_homo,     ]).override('cache_percent', pg(1,2,1) + pg(4,12,4) + [0.16]).override('batchsize', [ 500]).override('use_amp', [True]))
 # cfg_list_collector.concat(cur_common_base.copy().override('dataset', [Dataset.mag240m_homo,     ]).override('cache_percent', pg(1,2,1) + pg(4,12,4) + [0.16]).override('batchsize', [1000]).override('use_amp', [True]))
